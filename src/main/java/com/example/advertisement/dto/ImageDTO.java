48825442d793a7552ff501a7504cfefd7d2851bc
first <pdh980927@naver.com>
2022-02-11 17:00:38 +0900

    querydsl problem

---
+++ src/main/java/com/example/advertisement/dto/ImageDTO.java
@@ -0,0 +1,11 @@
+package com.example.advertisement.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ImageDTO {
+    Long imageId;
+    Long motherId;
+    String imageName;
+    String imageData;
+}
